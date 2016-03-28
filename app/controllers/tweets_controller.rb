da6399c4bfb8b9babc7754f71544fdc18c2e259c
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-03-28 17:25:03 +0200

    add tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,24 @@
 class TweetsController < ApplicationController
   # GET /tweets
   def index
-    @users = Tweet.all
+    @tweet = current_user.tweets.build if signed_in?
+    @feed_items = Tweet.all.paginate(page: params[:page])
+    render 'static_pages/home'
   end
+
+  def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      flash[:danger] = @tweet.errors.full_messages.to_sentence
+      redirect_to root_url
+    end
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
 end
