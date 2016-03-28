b4eeb479f7180d9541de48b7ea6a06f30efd0996
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-03-29 00:04:46 +0200

    add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @tweet = current_user.tweets.build if user_signed_in?
+    @feed_items = User.find_by!(slug: params[:id]).tweets.paginate(page: params[:page])
+  end
 end
